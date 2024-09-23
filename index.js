6a8e43b3cdb19afe09557812f02cf39c709a1cff
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-09-23 23:40:14 +0200

    Created Branch instances and compared them

--- index.js
+++ index.js
@@ -19,18 +19,18 @@ class BankBranch {
   }
 }
 
-// 3. In the `BankBranch` class:
-//    - Create a constructor that takes `branchInfo` as a parameter.
-//    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
-//    - If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
-//    - Return the `bankBranchInstance` whether it's newly created or existing.
 
-// 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
 
-// 5. In the usage section:
-//    - Create instances of the `BankBranch` class, such as `branchA` and `branchB`, with different branch information.
-//    - Use the `getBranchInfo` method to retrieve branch information from the instances.
-//    - Verify that `branchA` and `branchB` are both referring to the same instance by comparing them using `===`.
+// 3. Create instances of the `BankBranch` class, such as `branchA` and `branchB`, with different branch information.
+
+let branchA = new BankBranch({ name: 'Branch A', address: '1238 Castle Road', city: 'CapeTown' });
+let branchB = new BankBranch({ name: 'Branch B', address: '4568 WillieCollins Street', city: 'Johannesburg' });
+
+// 4. Verify that `branchA` and `branchB` are both referring to the same instance by comparing them using `===`.
+
+console.log(branchA === branchB);
+console.log(branchA)
+console.log(branchB)
 
 // This pseudo-code provides a step-by-step explanation of how to implement the Singleton Pattern for managing a single instance of the `BankBranch` class throughout the application, ensuring that multiple instances refer to the same object.
 
